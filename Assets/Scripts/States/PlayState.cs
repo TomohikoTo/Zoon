5ac0a400760857e05750fffb59e0d1b68fffd899
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-11-07 13:38:32 +0900

    playStateの復旧

---
+++ Assets/Scripts/States/PlayState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayState : StateInt {
+	// GameStateManagerのインスタンスを再利用
+	private GameStateManager manager;
+	public PlayerStateManager psm;
+
+		
+
+	public PlayState(GameStateManager GSManager) {
+		//初期化
+		manager = GSManager;
+
+	}
+	
+	public void StateUpdate() { 
+		psm = GameObject.Find ("Player").GetComponent<PlayerStateManager>();
+		//更新処理
+		if( psm.ReachGoal == true){
+			Application.LoadLevel("Result");
+			Time.timeScale = 1;
+			manager.SwitchState(new ResultState(manager));  
+			psm.ReachGoal = false;
+
+		}
+		
+	}
+	
+	public void Render() { 
+		//描画等
+
+
+	}
+}
