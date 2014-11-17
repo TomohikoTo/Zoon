75b3accc9d68e61ac789662e2d070abe37076838
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-11-17 16:57:09 +0900

    stateManagerのスクリプトを単体テスト用にスクリプト編集

---
+++ Assets/Scripts/Controller/GameStateManagerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[Serializable]
+public class GameStateManagerController  {
+
+
+	public class IntStateManagerController ismcon;
+	//ゲームの状態を保持
+	public GameStateManager gsm;
+	public  GameStateManagerController() {
+		gsm = new GameStateManager
+
+	}
+
+
+	
+	
+}
