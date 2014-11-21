6c8ae66064c4e4057d6d0e9555c70d61b6015271
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-11-21 17:04:50 +0900

    GameManagerController単体テストのState遷移テスト

--- Assets/UnityTestTools/Editor/GameStateManagerTest.cs
+++ Assets/UnityTestTools/Editor/GameStateManagerTest.cs
@@ -7,6 +7,7 @@ public class GameStateManagerTest{
 
 	public IGameStateManagerController igsmcon;
 	public GameStateManagerController gsmcon;
+	public GameStateManager gsm;
 	[SetUp] public void Init()
 	{
 		this.igsmcon = GetEffectMock ();
@@ -19,22 +20,26 @@ public class GameStateManagerTest{
 	[Test]
 	[Category ("SwitchTitleState")]
 	public void SwitchTitleStateTest() {
-	
+		string activeState = gsmcon.gsm.SwitchState( new TitleState(this.gsm));
+		Assert.That ("TitleState", Is.EqualTo (activeState));
 	}
 	[Test]
 	[Category ("SwitchMenuState")]
 	public void SwitchMenuStateTest() {
-		
+		string activeState = gsmcon.gsm.SwitchState( new MenuState(this.gsm));
+		Assert.That ("MenuState", Is.EqualTo (activeState));
 	}
 	[Test]
 	[Category ("SwitchPlayState")]
 	public void SwitchPlayStateTest() {
-		
+		string activeState = gsmcon.gsm.SwitchState( new PlayState(this.gsm));
+		Assert.That ("PlayState", Is.EqualTo (activeState));
 	}
 	[Test]
 	[Category ("SwitchResultState")]
 	public void SwitchResultStateTest() {
-		
+		string activeState = gsmcon.gsm.SwitchState( new ResultState(this.gsm));
+		Assert.That ("ResultState", Is.EqualTo (activeState));
 	}
 
 	private IGameStateManagerController GetEffectMock () {
