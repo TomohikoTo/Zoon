989c61fe521375b06964b5d063ce05d9bfe495cc
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-11-27 16:57:51 +0900

    ＡＩStateの修正

--- Assets/Scripts/Controller/AIState.cs
+++ Assets/Scripts/Controller/AIState.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 namespace zoon {
 public class AIState : MonoBehaviour , IPlayerState{
-
-
-		public AIState(PlayerStateManager PSManager) {
+	public MouseMover mm;
+	public MouseController mcon;
+	Random actRandom = new Random();
+	private	float DicideAct = 0;
+	public AIState(PlayerStateManager PSManager) {
 			//初期化
 
 			
@@ -22,7 +24,10 @@ public class AIState : MonoBehaviour , IPlayerState{
 	}
 
 	public void StateUpdate(){
+			mm.
+			SetRandom();
 			keyJammer();
+			DoAct();
 
 	}
 	
@@ -30,6 +35,20 @@ public class AIState : MonoBehaviour , IPlayerState{
 	
 	}
 
+	public void SetRandom(){
+		DicideAct = Random.Range(1.0f, 9.0f);
+	}
+	public void DoAct(){
+		if( 1.0f <= DicideAct && DicideAct < 3.0f ){
+				mcon.PressLeftArrow();
+			} else if( 3.0f <= DicideAct && DicideAct < 5.0f ){
+				mcon.PressRightArrow();
+			} else if( 5.0f <= DicideAct && DicideAct < 7.0f ){
+				mcon.PressUpArrow();
+			} else if( 7.0f <= DicideAct && DicideAct <= 9.0f ){
+				mm.OnEnable();
+			}
+	}
 	public bool keyJammer(){
 			if(Input.anyKey){
 				
@@ -38,6 +57,7 @@ public class AIState : MonoBehaviour , IPlayerState{
 			}
 			return true;
 	}
+
 }
 
 }
