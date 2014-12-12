6434cedd784f993ad82152e03db76c1c3de00663
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-12-12 23:59:10 +0900

    no message

---
+++ Assets/Scripts/ScoreData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreData : MonoBehaviour {
+
+	private float score = 0.0f;
+	// Use this for initialization
+	void Start () {
+
+		score = 0.0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//ポイント獲得
+	public void AddScore(float ScorePoint){
+		score += ScorePoint;
+	}
+
+	public float GetScore(){
+		return score;
+	}
+	public void SetScore (float score) {
+		this.score = score;
+	}
+}
