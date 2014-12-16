916a5725347103a55e5a89525d513c548b1e6c54
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-12-16 14:51:53 +0900

    制限時間が0になると終了するようスクリプトを追加

--- Assets/Scripts/States/PlayState.cs
+++ Assets/Scripts/States/PlayState.cs
@@ -9,6 +9,7 @@ public class PlayState : IState {
 	private GameStateManager manager;
 	public PlayerStateManager psm;
 	public LifeDisplayer ld;
+	public TimerDisplayer td;
 	public bool PositionReset = false;
 
 
@@ -17,20 +18,38 @@ public class PlayState : IState {
 		manager = GSManager;
 			psm = GameObject.Find ("Player").GetComponent<PlayerStateManager>();
 			ld	= GameObject.Find ("Life").GetComponent<LifeDisplayer>();
+			td	= GameObject.Find ("Timer").GetComponent<TimerDisplayer>();
 			Life = ld.GetLife();
 	}
 	public void StateUpdate() { 
 
 		
 		//更新処理
-		if( ld.GetLife() <= 0){
-			Application.LoadLevel("Result");
-			Time.timeScale = 1;
-	manager.SwitchState(new ResultState(manager));  
-	
-		}
+			TimeCourse();
+			GameEnd();
 	}
-		
+
+		public void GameEnd(){
+			if( ld.GetLife() <= 0){
+				Application.LoadLevel("Result");
+				Time.timeScale = 1;
+				manager.SwitchState(new ResultState(manager));  
+				
+			}
+
+			if( td.GetTime() <= 0){
+				Application.LoadLevel("Result");
+				Time.timeScale = 1;
+				manager.SwitchState(new ResultState(manager));  
+				
+			}
+
+		}
+
+		public void  TimeCourse(){
+
+			td.ReduceTime();
+		}
 		
 	
 
