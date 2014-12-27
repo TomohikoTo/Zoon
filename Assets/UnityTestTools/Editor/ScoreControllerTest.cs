e3a725f64803bbaa64f08d6f6aee55c37d0b6e52
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-12-27 16:50:15 +0900

    単体テスト: テスト項目の追加

--- Assets/UnityTestTools/Editor/ScoreControllerTest.cs
+++ Assets/UnityTestTools/Editor/ScoreControllerTest.cs
@@ -6,7 +6,7 @@ namespace zoon {
 	[TestFixture]
 	[Category ("Player Test")]
 	public class ScoreControllerTest {
-
+		public float ScorePoint = 10f;
 		public IScoreController iscon;
 		public ScoreController scon;
 		public ScoreDisplayer sd;
@@ -22,7 +22,39 @@ namespace zoon {
 
 		[Test]
 		[Category ("Test")]
+		public void ReduceScoreErrorCheckTest() {
+			//点数がマイナスにならないかどうかのテスト
+			scon.AddScore(ScorePoint);
+			for( int i = 0; i < 100 ; i++)
+			{
+				scon.ReduceScore(ScorePoint);
+			}
+			Assert.That (0f, Is.EqualTo (scon.GetScore() ));
+		}
 
+		[Test]
+		[Category ("ErrorCheckTest")]
+		public void AddErrorCheckTest() {
+			//点数が上限を超えて異常な値になるかどうかのテスト
+			ScorePoint = 9999999999f;
+			for( int i = 0; i < 5000 ; i++)
+			{
+				scon.AddScore(ScorePoint);
+			}
+			Assert.That (0f, Is.EqualTo (scon.GetScore() ));
+		}
+
+		[Test]
+		[Category ("Test")]
+		public void StringCheckTest() {
+			//点数がマイナスにならないかどうかのテスト
+			scon.AddScore(ScorePoint);
+			for( int i = 0; i < 100 ; i++)
+			{
+				scon.ReduceScore(ScorePoint);
+			}
+			Assert.Fail ();
+		}
 		private IScoreController GetEffectMock () {
 			return Substitute.For<IScoreController> ();
 		}
