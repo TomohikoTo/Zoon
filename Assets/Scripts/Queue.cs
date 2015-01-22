ceb57ec83ce3d11b332bf9bb0ad36d722f148524
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-01-22 23:54:25 +0900

    Queueスクリプト修正

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class Queue  {
 
+	int QUEUE_MAX = 5;
+	int QUEUE_MIN = -1;
+	int queue_first = 0;	//キューの先頭
+	int queue_last = 0;		//キューの末尾
 
 	//キューにデータを追加する
 	public void enqueue(){
+
+		if( (queue_last + 1) %QUEUE_MAX == queue_first)
+		{
+
+		}
+		else
+		{
+
+		}
 	}
 
 	//キューのデータを取り出す
