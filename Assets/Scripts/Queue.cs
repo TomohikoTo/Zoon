bff2e1040c1ec40882b982db4b4ee758475c4df7
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-01-23 23:36:55 +0900

    Queueスクリプト修正

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class Queue  {
 
-	int QUEUE_MAX = 5;
-	int QUEUE_MIN = -1;
+	int QUEUE_MAX = 5;		//キューの要領
+	int QUEUE_MIN = -1;		//キューの空
+	int queue[QUEUE_MAX];
 	int queue_first = 0;	//キューの先頭
 	int queue_last = 0;		//キューの末尾
 
@@ -23,6 +24,20 @@ public class Queue  {
 
 	//キューのデータを取り出す
 	public void dequeue(){
+
+		int queue_return;
+
+		if(queue_first == queue_last)
+		{
+			return QUEUE_EMPTY;
+		}
+		else
+		{
+			queue_return = queue[queue_fisrt];
+
+			queue_first = (queue_first + 1)%QUEUE_MAX;
+			return queue_return;
+		}
 	}
 	
 
