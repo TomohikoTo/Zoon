30522e1f1ccc8cd3f21698c5530fd0349b6aa237
TomohikoT <idumi_toilet@yahoo.co.jp>
2015-02-04 23:44:58 +0900

    参考用のスクリプト

--- Assets/Scripts/NodeSample.cs
+++ Assets/Scripts/NodeSample.cs
@@ -3,13 +3,117 @@ using System.Collections;
 
 public class NodeSample : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	class Node
+		
+	{
+		
+		public int item;
+		
+		public Node leftc;
+		
+		public Node rightc;
+		
+		public void display()
+			
+		{
+			
+			Console.Write("[");
+			
+			Console.Write(item);
+			
+			Console.Write("]");
+			
+		}
+		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+	class Tree
+		
+	{
+		
+		public Node root;
+		
+		public Tree()
+			
+		{ 
+			
+			root = null; 
+			
+		}
+		
+		public Node ReturnRoot()
+			
+		{
+			
+			return root;
+			
+		}
+		
+		public void Insert(int id)
+			
+		{
+			
+			Node newNode = new Node();
+			
+			newNode.item = id;
+			
+			if (root == null)
+				
+				root = newNode;
+			
+			else
+				
+			{
+				
+				Node current = root;
+				
+				Node parent;
+				
+				while (true)
+					
+				{
+					
+					parent = current;
+					
+					if (id < current.item)
+						
+					{
+						
+						current = current.leftc;
+						
+						if (current == null)
+							
+						{
+							
+							parent.leftc = newNode;
+							
+							return;
+							
+						}
+						
+					}
+					
+					else
+						
+					{
+						
+						current = current.rightc;
+						
+						if (current == null)
+							
+						{
+							
+							parent.rightc = newNode;
+							
+							return;
+							
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+		}
 }
